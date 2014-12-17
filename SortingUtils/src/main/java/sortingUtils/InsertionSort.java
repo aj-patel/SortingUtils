ad35dc71025d20a452ed62bb521cefb6e6d1acb4
Ajay Patel <connecttoaj@gmail.com>
2014-12-17 15:06:53 +0530

    added insertion sort to the utility

--- SortingUtils/src/main/java/sortingUtils/InsertionSort.java
+++ SortingUtils/src/main/java/sortingUtils/InsertionSort.java
@@ -1,5 +1,7 @@
 package sortingUtils;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * This is the interface for various sorting techniques.
  * @author ajay.patel
@@ -7,10 +9,29 @@ package sortingUtils;
  */
 public class InsertionSort 
 {
-	
-	public int[] sortIntArray(int[] input){
-
-		return null;	
+	public static int[] sortIntArray(final int[] input){
+		long startTime = System.nanoTime();
+		int key;
+		int j;
+		
+		for(int i=1;i<input.length;i++){
+			key=input[i];
+			j=i-1;
+			while(j>=0 && key < input[j]){
+				input[j+1]=input[j];
+				j--;
+			}
+			input[j+1]=key;
+		}
+		long timeTakenNano = System.nanoTime()-startTime;
+		long timeTakenSec = TimeUnit.SECONDS.convert(timeTakenNano,TimeUnit.NANOSECONDS);
+		if(timeTakenSec ==0){
+			System.out.println("Time taken for sorting is :"+timeTakenNano+" nanoseconds.");
+		}
+		else{
+			System.out.println("Time taken to sort is :" +timeTakenSec);
+		}
+		return input;	
 		
 	}
 }
