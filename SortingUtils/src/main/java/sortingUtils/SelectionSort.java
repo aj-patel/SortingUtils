35bf7ce161f7ab305d65f0aa3f526d9c03634d2d
Ajay Patel <connecttoaj@gmail.com>
2014-12-17 18:16:36 +0530

    added selectionsort

---
+++ SortingUtils/src/main/java/sortingUtils/SelectionSort.java
@@ -0,0 +1,52 @@
+package sortingUtils;
+
+import java.util.concurrent.TimeUnit;
+
+/***
+ * 
+ * @author ajay.patel
+ * This is a utility for sorting using Selection Sort technique.
+ */
+public class SelectionSort {
+	
+	/**
+	 * API for sorting integer array through SelectionSort technique.
+	 * @param input[]
+	 * @return sorted array
+	 */
+	public static int[] sortIntArray(int [] input){
+		long startTime = System.nanoTime();
+		int key;
+		for(int i=0;i<input.length;i++){
+			key = i;
+			for(int j=i+1;j<input.length;j++){
+				if(input[j]< input[key]){
+					key=j;
+				}
+			}
+			swap(input,i,key);
+		}
+		long timeTakenNano = System.nanoTime()-startTime;
+		long timeTakenSec = TimeUnit.SECONDS.convert(timeTakenNano,TimeUnit.NANOSECONDS);
+		if(timeTakenSec ==0){
+			System.out.println("Time taken for sorting is :"+timeTakenNano+" nanoseconds.");
+		}
+		else{
+			System.out.println("Time taken to sort is :" +timeTakenSec);
+		}
+		return input;
+	}
+	
+	/**
+	 * 
+	 * @param input
+	 * @param fromIdex
+	 * @param toIndex
+	 * This is for swapping elements.
+	 */
+	private static void swap(int[] input, int fromIdex, int toIndex){
+		int temp = input[fromIdex];
+		input[fromIdex] = input[toIndex];
+		input[toIndex] = temp;
+	}
+}
