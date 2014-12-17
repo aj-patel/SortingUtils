3f86b01be61df0f50f0003b430add456fd34a927
Ajay Patel <connecttoaj@gmail.com>
2014-12-17 14:10:19 +0530

    added insertion sort technique

---
+++ SortingUtils/src/main/java/sortingUtils/InsertionSort.java
@@ -0,0 +1,16 @@
+package sortingUtils;
+
+/**
+ * This is the interface for various sorting techniques.
+ * @author ajay.patel
+ *
+ */
+public class InsertionSort 
+{
+	
+	public int[] sortIntArray(int[] input){
+
+		return null;	
+		
+	}
+}
