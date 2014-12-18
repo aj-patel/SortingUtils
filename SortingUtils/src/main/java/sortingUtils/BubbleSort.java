7424b0ab869b4e87ad315e998363937bff328952
Ajay Patel <connecttoaj@gmail.com>
2014-12-18 11:41:30 +0530

    improved version and added proper comments

--- SortingUtils/src/main/java/sortingUtils/BubbleSort.java
+++ SortingUtils/src/main/java/sortingUtils/BubbleSort.java
@@ -1,20 +1,36 @@
 package sortingUtils;
 
+import java.util.concurrent.TimeUnit;
+
 /***
  * For sorting elements using BubbleSort technique.
- * 
+ * To be used for relatively small size of elements. Adaptable, stable, in-place.
+ * Worst case and average case is O(n*n). However O(n) when array is already sorted.
  * @author ajay.patel
  */
 public class BubbleSort {
 	
 	public static int[] sortIntArray(int[] input){
-		
+		long startTime = System.nanoTime();
+		boolean swapped = false;
 		for(int i=0;i<input.length-1;i++){
 			for(int j=0;j<input.length-1;j++){
 				if(input[j] > input[j+1]){
 					swap(input, j, j+1);
+					swapped = true;
 				}
 			}
+			if(!swapped){
+				break;
+			}
+		}
+		long timeTakenNano = System.nanoTime()-startTime;
+		long timeTakenSec = TimeUnit.SECONDS.convert(timeTakenNano,TimeUnit.NANOSECONDS);
+		if(timeTakenSec ==0){
+			System.out.println("Time taken for sorting is :"+timeTakenNano+" nanoseconds.");
+		}
+		else{
+			System.out.println("Time taken to sort is :" +timeTakenSec);
 		}
 		return input;
 	}
